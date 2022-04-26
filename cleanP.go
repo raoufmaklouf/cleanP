837bad09c5bbcbe6476786a9af8cfb4ec4ac320e
raoufmaklouf <raoufmaklouf0int@gmail.com>
2022-04-26 16:11:50 +0000

    Update cleanP.go

--- cleanP.go
+++ cleanP.go
@@ -5,27 +5,38 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
+
+var static_exts = []string{"js", "css", "png", "jpg", "jpeg", "svg", "ico", "webp", "ttf", "otf", "woff", "gif", "pdf", "bmp", "eot", "mp3", "woff2", "mp4", "avi"}
 
 func main() {
 	cont := []string{}
 	uniqPath := []string{}
+	deepcheklist := []string{}
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
 
 		line := sc.Text()
-		regex, _ := regexp.MatchString("[?]", line)
-		if regex == true {
-			urlInjected := injectparam(line)
-			pathInjected := uniqurl(line)
-			if contains(cont, urlInjected) == false {
-				if contains(uniqPath, pathInjected) == false {
-					uniqPath = append(uniqPath, pathInjected)
-					cont = append(cont, urlInjected)
-					fmt.Println(line)
-				}
+		regex1, _ := regexp.MatchString("[?]", line)
+		regex2, _ := regexp.MatchString("=", line)
+		if regex1 == true && regex2 == true {
+			if isStatic(line) == false {
+				urlInjected := injectparam(line)
+				pathKey := uniqurl(line)
+				deepChekey := deepchek(line)
+				if contains(cont, urlInjected) == false {
+					if contains(uniqPath, pathKey) == false {
+						if contains(deepcheklist, deepChekey) == false {
+							deepcheklist = append(deepcheklist, deepChekey)
+							uniqPath = append(uniqPath, pathKey)
+							cont = append(cont, urlInjected)
+							fmt.Println(line)
+						}
+					}
 
+				}
 			}
 
 		}
@@ -68,11 +79,49 @@ func injectparam(url string) (finalResolt string) {
 	}
 	return
 }
+func deepchek(url string) (finalkey string) {
+	var key string
+	baseul := strings.Split(url, "?")[0]
+	domain := strings.Split(url, "/")[2]
+	parameters := strings.Split(url, "?")[1]
+	regex2, _ := regexp.MatchString("&", parameters)
+	key = domain + ":"
+	if regex2 == true {
+		for _, i := range strings.Split(parameters, "&") {
+			paramkey := strings.Split(i, "=")[0]
+			key = key + paramkey + ":"
 
+		}
+	} else {
+		paramkey := strings.Split(parameters, "=")[0]
+		key = key + paramkey + ":"
+
+	}
+	for _, Path := range strings.Split(baseul, "/") {
+		key = key + strconv.Itoa(len(Path)) + ":"
+
+	}
+	finalkey = key
+	return
+
+}
+
+func isStatic(url string) bool {
+	Url := strings.Split(url, "?")[0]
+	var static bool = false
+	for _, ext := range static_exts {
+		if strings.HasSuffix(Url, ext) == true {
+			static = true
+			break
+		}
+	}
+	return static
+}
 func contains(s []string, str string) bool {
 	for _, v := range s {
 		if v == str {
 			return true
+			break
 		}
 	}
 
